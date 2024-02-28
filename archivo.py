896d6332977a4de268b6dbe6faa93d06131ceb50
kiarayodko <145400769+kiarayodko@users.noreply.github.com>
2024-02-28 18:29:05 -0300

    agregue parte del punto 2

--- archivo.py
+++ archivo.py
@@ -10,6 +10,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from inline_sql import sql, sql_val
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 data = pd.read_csv("sign_mnist_train.csv")
 
@@ -48,3 +51,48 @@ for fila in range(filas):
         axes[fila][columna].tick_params(left = False, right = False , labelleft = False , 
                 labelbottom = False, bottom = False)  
 
+#%%
+### PUNTO 2 ###
+
+# Construimos un nuevo dataframe solo con aquellas imagenes que sean una A o una L
+consulta="""
+SELECT *
+FROM data
+WHERE label==0 OR label==11
+"""
+data_A_L=sql^consulta
+
+#◙ Vemos el numero total de muestras 
+total_muestras_A_L=len(data_A_L)
+print(total_muestras_A_L)
+
+# Vemos si hay una cantidad parecida de señas A y L 
+
+cant_A = data_A_L['label'].value_counts()[0]
+cant_L = data_A_L['label'].value_counts()[11]
+
+porcentaje_A=cant_A/total_muestras_A_L*100
+print(porcentaje_A)
+porcentaje_L=cant_L/total_muestras_A_L*100
+print(porcentaje_L)
+
+#declaramos las variables
+n=5
+X = data_A_L.drop('label', axis=1).sample(n, axis=1) #solo tomo n atributos de X
+y = data_A_L['label']
+
+
+#Separamos en casos de train(70%) y test(30%)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=159)
+
+# Declaramos el tipo de modelo
+k=5
+neigh = KNeighborsClassifier(n_neighbors = k)
+
+# Entrenamos el modelo
+neigh.fit(X_train, y_train)
+
+# Evaluamos los resultados
+
+r2 = neigh.score(X_test, y_test)
+print("R^2 (test)", r2)
