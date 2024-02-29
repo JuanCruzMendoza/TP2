1cd69041808fbc4c9405654594f2e65cbc9844d1
kiarayodko <145400769+kiarayodko@users.noreply.github.com>
2024-02-29 11:48:45 -0300

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -24,7 +24,7 @@ imagenes = data.drop(columns="label")
 labels = data["label"].unique()
 labels.sort()
 
-diccionario_letras = {0:"A", 1:"B", 2:"C", 3:"D", 4:"E", 5:"F", 6:"G", 7:"H", 8:"I", 9:"J", 10:"K", 11:"L", 12:"M", 13:"N", 14:"O", 15:"P", 16:"Q", 17:"R", 18:"S", 19:"T", 21:"U", 21:"V", 22:"W", 23:"X", 24:"Y", 25:"Z"}
+diccionario_letras = {0:"A", 1:"B", 2:"C", 3:"D", 4:"E", 5:"F", 6:"G", 7:"H", 8:"I", 9:"J", 10:"K", 11:"L", 12:"M", 13:"N", 14:"O", 15:"P", 16:"Q", 17:"R", 18:"S", 19:"T", 20:"U", 21:"V", 22:"W", 23:"X", 24:"Y", 25:"Z"}
 
 # Nos fijamos si los datos estan balanceados
 # Contamos la cantidad de datos por cada etiqueta
